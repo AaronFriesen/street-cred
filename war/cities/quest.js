4fae6422d267972aba389c81f05596a2e6022bf3
hammy55 <evan.cahill55@gmail.com>
2013-11-05 15:05:34 -0500

    Reparse JSON for all cities, New Quest functionality, Final City Template

--- war/cities/quest.js
+++ war/cities/quest.js
@@ -1,43 +1,54 @@
 $(function() {
+  var questdata_json;
   var questdata;
   var $table = $("#quests");
   var template = Mustache.compile($("#template").html());
   $.getJSON($("#quests").data("json"), function(data) {
+    data.quests.sort(level_sort);
     questdata = data;
-    questdata.quests.sort(level_sort);
+    questdata_json = JSON.stringify(data);
     $table.children("tbody").html(template(questdata));
   });
 
   //Handle errors from api call
   var $api_error = $("#api-error");
-  $api_error.bind("transitionend", function() {
+  $api_error.on("transitionend", function() {
     $api_error.hide();
-    $api_error.removeClass("fade-out");
   });
 
   $("button", $api_error).click(function(e) {
     $api_error.addClass("fade-out");
   });
+  
+  var $api_success = $("#api-success");
+  $api_success.on("transitionend", function() {
+    $api_success.hide();
+  });
+  
+  $("button", $api_success).click(function(e) {
+    $api_success.hide();
+  });
 
   //Handle api call
   $("#submit").click(function(e) {
+    e.preventDefault();
     var btn = $(this);
     btn.button('loading');
-    e.preventDefault();
     var server = $("#server").val();
     var username = $("#username").val();
 
     var timeout = setTimeout(function() {
       btn.button('reset');
       $("span", $api_error).text(username + " at " + server);
-      $api_error.show();
+      $api_error.removeClass("fade-out").show();
     }, 5000);
 
     $.getJSON("http://us.battle.net/api/wow/character/" + encodeURIComponent(server) + "/" + encodeURIComponent(username) + "?fields=reputation&jsonp=?", 
     function(data) {
       clearTimeout(timeout);
+      btn.button('reset');
+      $api_error.hide();
       $("#character-form").hide();
-      $("#character-info").show();
       var i;
       for (i = 0; i < data.reputation.length; i++) {
         if (data.reputation[i].id === $("#rep-progress").data("city-id")) break;
@@ -50,29 +61,45 @@ $(function() {
       $("#points-left").html(rep.max - rep.value);
 
       //Remove quests that are not availabe for this character
+      console.log(data.class + " " + data.race);
       for (var j = 0; j < questdata.quests.length; j++) {
-        if (questdata.quests[j].req > data.level) {
+        var quest = questdata.quests[j];
+        if (quest.req > data.level) {
+          questdata.quests.splice(j--, 1);
+        } else if (quest.class && !contains(data.class, quest.class)) {
+          questdata.quests.splice(j--, 1);  
+        } else if (quest.race && !contains(data.race, quest.race)) {
           questdata.quests.splice(j--, 1);
         }
       }
       $table.children("tbody").html(template(questdata));
 
+      $("#rep-progress").children().removeClass("progress-bar-danger progress-bar-success progress-bar-warning");
       if (rep.standing < 3) $("#rep-progress").children().addClass("progress-bar-danger");
       else if (rep.standing > 3) $("#rep-progress").children().addClass("progress-bar-success");
       else $("#rep-progress").children().addClass("progress-bar-warning");
+      
+      $("#username-fill").text(username.charAt(0).toUpperCase() + username.slice(1));
+      $("#character-info").show();
+      
       $("#rep-progress").children().css("width", data.reputation[i].value / data.reputation[i].max * 100 + "%");
+      
+      setTimeout(function() {
+        $api_success.addClass("fade-out");
+      }, 5000);
+      $api_success.removeClass("fade-out").show();
     });
   });
 
   //Attach panel collapse toggles
-  $(".panel-collapse").on("click", function() {
+  $(".panel-collapse").click(function() {
     $(this).children("button").html(($(this).hasClass("collapsed")) ? "&minus;" : "&plus;");
   });
 
   //Attach sort listeners to headers
   var $headers = $("th", $table);
   var sorted = 1; //The current column that is sorted;
-  var direction; //true -> ascending false -> descending
+  var direction =  true; //true -> ascending false -> descending
   $headers.eq(0).click(function() {
     if (sorted === 0) {
       questdata.quests.reverse();
@@ -80,11 +107,7 @@ $(function() {
       direction = !direction;
     }
     else {
-      questdata.quests.sort(function(a, b) {
-        if (a.name < b.name) return -1;
-        else if (a.name > b.name) return 1;
-        else return 0;
-      });
+      questdata.quests.sort(name_sort);
       $headers.removeClass("sorted-up sorted-down");
       direction = true;
       sorted = 0;
@@ -117,6 +140,7 @@ $(function() {
     }
     else {
       questdata.quests.sort(function(a, b) {
+        if (a.req === b.req) return name_sort(a,b);
         return a.req - b.req;
       });
       $headers.removeClass("sorted-up sorted-down");
@@ -135,6 +159,7 @@ $(function() {
     }
     else {
       questdata.quests.sort(function(a, b) {
+        if (a.rep === b.rep) return name_sort(a,b);
         return a.rep - b.rep;
       });
       $headers.removeClass("sorted-up sorted-down");
@@ -153,6 +178,7 @@ $(function() {
     }
     else {
       questdata.quests.sort(function(a, b) {
+        if (a.exp === b.exp) return name_sort(a,b);
         return a.exp - b.exp;
       });
       $headers.removeClass("sorted-up sorted-down");
@@ -162,8 +188,54 @@ $(function() {
     $(this).addClass(direction ? "sorted-up" : "sorted-down");
     $table.children("tbody").html(template(questdata));
   });
+  
+  $headers.eq(5).click(function() {
+    if (sorted === 5) {
+      questdata.quests.reverse();
+      $(this).removeClass("sorted-up sorted-down");
+      direction = !direction;
+    }
+    else {
+      questdata.quests.sort(function(a, b) {
+        if (a.cat < b.cat) return -1;
+        else if (a.cat > b.cat) return 1;
+        else return name_sort(a,b);
+      });
+      $headers.removeClass("sorted-up sorted-down");
+      direction = true;
+      sorted = 5;
+    }
+    $(this).addClass(direction ? "sorted-up" : "sorted-down");
+    $table.children("tbody").html(template(questdata));
+  });
+  
+  //Handle reset of character form
+  $("#reset").click(function(e) {
+    e.preventDefault();
+    $("#character-info").hide();
+    questdata = JSON.parse(questdata_json);
+    $headers.removeClass("sorted-up sorted-down");
+    $headers.eq(1).addClass("sorted-up");
+    sorted = 1;
+    direction = true;
+    $table.children("tbody").html(template(questdata));
+    $("#character-form").show();
+  });
 });
 
 function level_sort(a, b) {
+  if (a.level === b.level) return name_sort(a,b);
   return a.level - b.level;
 };
+
+function name_sort(a,b) {
+  if (a.name < b.name) return -1;
+  else if (a.name > b.name) return 1;
+  else return 0;
+}
+
+function contains(id, arr) {
+  for (i = 0; i < arr.length; i++)
+    if (id === arr[i].id) return true;
+  return false;
+}
